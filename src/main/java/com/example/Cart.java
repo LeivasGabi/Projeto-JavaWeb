2da0f5567cc7180155cbb7c3d0018cc743cb5349
Gabrielle Pinto Leivas <GABRIELLEPL18@edu.unisinos.br>
2020-01-06 15:34:05 -0300

    adicionado carrinho

---
+++ src/main/java/com/example/Cart.java
@@ -0,0 +1,65 @@
+package com.example;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.ManyToOne;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.annotation.Id;
+
+@EntityScan
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @ManyToOne
+    private Product product;
+    @ManyToOne
+    private Customer customer;
+
+    protected Cart() {}
+    public Cart (Product product, Customer customer) {
+        this.product = product;
+        this.customer = customer;
+    }
+    @Override
+    public String toString() {
+        return String.format("{id=%d, custumer='%s', product='%s'}", id, customer, product);
+    }
+    /**
+     * @return the customer
+     */
+    public Customer getCustomer() {
+        return customer;
+    }
+    /**
+     * @return the product
+     */
+    public Product getProduct() {
+        return product;
+    }
+    /**
+     * @param customer the customer to set
+     */
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+    /**
+     * @param product the product to set
+     */
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+}
